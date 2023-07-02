8b56dafbf7830bd8e3434f9458db678f7887ad0b
bilal-288 <73030325+bilal-288@users.noreply.github.com>
2023-07-02 17:20:08 +0200

    Complete sample task

---
+++ resources/views/backendLayouts/footer.blade.php
@@ -0,0 +1,18 @@
+    <div class="footer-bottom-area">
+        <div class="container" style="text-align:center">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="copyright" style="margin-top: 2vh;">
+                    <p>&copy; 2022 Hello pine. All Rights Reserved. <a href="https://www.hello-pine.com/" target="_blank">hello-pine.de</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End footer bottom area -->
+   
+     <!-- Latest jQuery form server -->
+     <script src="https://code.jquery.com/jquery.min.js"></script>
+    
+    <!-- Bootstrap JS form CDN -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+ 
